14f42286e32b5ca48c03d724be9986b92db2fa13
pnpn111 <pnpn111@qq.com>
2023-04-03 13:46:49 +0800

    增加用于检测公网连接的具体代码；完善wooden_start.cpp。

--- wooden_start.h
+++ wooden_start.h
@@ -1,5 +1,7 @@
 #ifndef WOODEN_GAME_WOODEN_START_H
 #define WOODEN_GAME_WOODEN_START_H
 void wooden_initialization();
-
+bool check_internet_connect();
+bool internet_connect;
 #endif //WOODEN_GAME_WOODEN_START_H
+
