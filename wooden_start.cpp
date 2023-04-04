112d897c80ef207557f36ce7737b4253c6730ed6
pnpn111 <31959528+pnpn111@users.noreply.github.com>
2023-04-04 13:30:03 +0800

    Update wooden_start.cpp

--- wooden_start.cpp
+++ wooden_start.cpp
@@ -9,46 +9,39 @@
 //通过ping检查公网连接，return 0表示ping成功
 bool check_internet_connect() {
     using namespace std;
-    try {
-        std::vector<std::string> v;
-        std::array<char, 128> buffer;
-        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("ping www.baidu.com -c 2 -w 2 ", "r"), pclose);
-        if (!pipe) {
-            throw std::runtime_error("popen() failed!");
-        }
-        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-            v.push_back(buffer.data());
-        }
-        //读取倒数第二行 2 packets transmitted, 2 received, 0% packet loss, time 1001ms
-        if (v.size() > 1) {
-            string data = v[v.size() - 2];
-            int iPos = data.find("received,");
-            if (iPos != -1) {
-                data = data.substr(iPos - 3, 3);//截取字符串返回packet loss
+    std::vector<std::string> v;
+    std::array<char, 128> buffer;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("ping www.baidu.com -c 2 -w 2 ", "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        v.push_back(buffer.data());
+    }
+    //读取倒数第二行 2 packets transmitted, 2 received, 0% packet loss, time 1001ms
+    if (v.size() > 1) {
+        string data = v[v.size() - 2];
+        int iPos = data.find("received,");
+        if (iPos != -1) {
+            data = data.substr(iPos - 3, 3);//截取字符串返回packet loss
+            int n = atoi(data.c_str());
+            if (n == 0)
+                return true;
+            else
+                return false;
+        } else {
+            int iPos2 = data.find("已接收");
+            if (iPos2 != -1) {
+                data = data.substr(iPos + 10, 3);//截取字符串返回packet loss
                 int n = atoi(data.c_str());
                 if (n == 0)
                     return true;
                 else
                     return false;
-            } else {
-                int iPos2 = data.find("已接收");
-                if (iPos2 != -1) {
-                    data = data.substr(iPos + 10, 3);//截取字符串返回packet loss
-                    int n = atoi(data.c_str());
-                    if (n == 0)
-                        return true;
-                    else
-                        return false;
-                }
             }
-        } else {
-            return true;
         }
-    } catch (std::exception e) {
-        string error_info = e.what();
-        cout<<error_info<<endl;
-        assert(1);
-        //ogWriteSql("1.checkNet() 判断是否有网络 error");
+    } else {
+        return true;
     }
     //cout<<"checkNet 4444: "<<endl;
 }
