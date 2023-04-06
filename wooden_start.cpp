5c35134ce36bbcbe1b0dd99b0295a3bde5571b47
pnpn111 <pnpn111@qq.com>
2023-04-06 13:47:04 +0800

    增加用于检测公网连接的具体代码；完善wooden_start.cpp；增加debug相关文件并移动dprint到文件中。

--- wooden_start.cpp
+++ wooden_start.cpp
@@ -1,11 +1,75 @@
+#include <vector>
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <iostream>
+#include <cassert>
+#include <array>
+
 #include "wooden_start.h"
+#include "wooden_debug.h"
+bool internet_connect;
+//通过ping检查公网连接，return 0表示ping成功
+bool check_internet_connect() {
+    using namespace std;
+    try {
+        std::vector<std::string> v;
+        std::array<char, 128> buffer;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("ping www.baidu.com -c 2 -w 2 ", "r"), pclose);
+        if (!pipe) {
+            throw std::runtime_error("popen() failed!");
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            dprint(buffer.data());
+            v.push_back(buffer.data());
+        }
+        //读取倒数第二行 2 packets transmitted, 2 received, 0% packet loss, time 1001ms
+        if (v.size() > 1) {
+            string data = v[v.size() - 2];
+            dprint(data);
+            int iPos = data.find("received,");
+            if (iPos != -1) {
+                data = data.substr(iPos - 3, 3);//截取字符串返回packet loss
+                dprint(data);
+                int n = atoi(data.c_str());
+                dprint(to_string(n));
+                if (n == 0)
+                    return true;
+                else
+                    return false;
+            } else {
+                int iPos2 = data.find("已接收");
+                if (iPos2 != -1) {
+                    data = data.substr(iPos + 10, 3);//截取字符串返回packet loss
+                    dprint(data);
+                    int n = atoi(data.c_str());
+                    dprint(to_string(n));
+                    if (n == 0)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        } else {
+            return true;
+        }
+    } catch (std::exception	e) {
+        string error_info = e.what();
+        dprint(error_info);
+        assert(1);
+        //ogWriteSql("1.checkNet() 判断是否有网络 error");
+    }
+    //cout<<"checkNet 4444: "<<endl;
+}
+
 
 //核心初始化
-void wooden_initialization()
-{
-    //检查网络连接
-
-    //
-
-    return;
+void wooden_initialization() {
+    //检查公网连接
+    if (check_internet_connect())dprint("无法访问公网");
+    {
+        internet_connect = true;
+        //检测内网连接
+    }
+    return ;
 }
--- wooden_start.h
+++ wooden_start.h
@@ -1,5 +1,6 @@
 #ifndef WOODEN_GAME_WOODEN_START_H
 #define WOODEN_GAME_WOODEN_START_H
 void wooden_initialization();
-
+extern bool internet_connect;
+bool check_internet_connect();
 #endif //WOODEN_GAME_WOODEN_START_H
