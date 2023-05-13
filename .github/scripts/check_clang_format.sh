8a465278158aa83078edea5fa931d3c2463513f6
Dell-G3 <Jestingaero@163.com>
2023-05-13 11:17:06 +0800

    未通过 clang-format 时返回预期的返回值

--- .github/scripts/check_clang_format.sh
+++ .github/scripts/check_clang_format.sh
@@ -7,8 +7,8 @@ mkdir -p ./clang-format
 for f in $files; do
     echo "进行代码文件 ${f} 的格式检查"
     command_failed=0
-    # result=$command_failed|$result
     clang-format -style=file --sort-includes -i --dry-run -Werror $f || command_failed=1
+    result=$command_failed|$result
     clang-format -style=file --sort-includes -i --dry-run -Werror $f >> ./clang-format/clang-format-output.log
     clang-format -style=file --sort-includes -i --dry-run -Werror $f 2>> ./clang-format/clang-format-error.log
     if [[ "$command_failed" == "1" ]]; then
