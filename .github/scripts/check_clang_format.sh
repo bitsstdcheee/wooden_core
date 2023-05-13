b26294db91cf4e76ac5445856bcb1e100ed50ca7
Dell-G3 <Jestingaero@163.com>
2023-05-13 11:23:22 +0800

    正确计算 result

--- .github/scripts/check_clang_format.sh
+++ .github/scripts/check_clang_format.sh
@@ -14,7 +14,7 @@ for f in $files; do
     echo "进行代码文件 ${f} 的格式检查"
     command_failed=0
     clang-format -style=file --sort-includes -i --dry-run -Werror $f || command_failed=1
-    result=$command_failed|$result
+    result=$(( $command_failed|$result ))
     clang-format -style=file --sort-includes -i --dry-run -Werror $f >> ./clang-format/clang-format-output.log
     clang-format -style=file --sort-includes -i --dry-run -Werror $f 2>> ./clang-format/clang-format-error.log
     if [[ "$command_failed" == "1" ]]; then
