2108bccde9c3159cb359b9e29ce36f8c74db9319
bitsstdcheee <1009770830@qq.com>
2023-06-14 15:43:47 +0800

    更改为使用 EXPECT_EQ

--- tests/judge_have/main.cpp
+++ tests/judge_have/main.cpp
@@ -8,25 +8,25 @@ using namespace tskl;
 #define make_weapon(x) make_pair(0, Skill(x, 1))
 
 TEST(have_att_test, Positive) {
-    EXPECT_TRUE(have_att(make_weapon(tskl::wooden_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::yellow_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::stone_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::iron_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::gold_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::diamond_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::enchanted_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::bo_sword)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::hither)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::fist)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::palm)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::tube)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::alpaca)));
-    EXPECT_TRUE(have_att(make_weapon(tskl::hither)));
+    EXPECT_EQ(have_att(make_weapon(tskl::wooden_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::yellow_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::stone_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::iron_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::gold_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::diamond_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::enchanted_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::bo_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::hither)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::fist)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::palm)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::tube)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::alpaca)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::hither)), true);
 }
 
 TEST(have_att_test, Negative) {
-    EXPECT_FALSE(have_att(make_weapon(tskl::clap)));
-    EXPECT_FALSE(have_att(make_weapon(tskl::defense)));
+    EXPECT_EQ(have_att(make_weapon(tskl::clap)), false);
+    EXPECT_EQ(have_att(make_weapon(tskl::defense)), false);
 }
 
 int main(int argc, char** argv) {
