7e40bb852ae2b370ba306e58f0d63f6cc95ec0bc
bitsstdcheee <1009770830@qq.com>
2023-06-16 21:34:13 +0800

    为 have_axe, have_clap_axe 添加测试

--- tests/judge_have/main.cpp
+++ tests/judge_have/main.cpp
@@ -29,6 +29,33 @@ TEST(have_att_test, Negative) {
     EXPECT_EQ(have_att(make_weapon(tskl::defense)), false);
 }
 
+TEST(have_axe_test, Positive) {
+    EXPECT_EQ(have_axe(make_weapon(tskl::wooden_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::normal_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::normal_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::diamond_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::enchanted_axe)), true);
+}
+
+TEST(have_axe_test, Negative) {
+    EXPECT_EQ(have_axe(make_weapon(tskl::clap)), false);
+    EXPECT_EQ(have_axe(make_weapon(tskl::defense)), false);
+}
+
+TEST(have_clap_axe_test, Positive) {
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::wooden_axe)), true);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::normal_axe)), true);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::normal_axe)), true);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::diamond_axe)), true);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::enchanted_axe)), true);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::clap)), true);
+}
+
+TEST(have_clap_axe_test, Negative) {
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::defense)), false);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::wooden_sword)), false);
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
