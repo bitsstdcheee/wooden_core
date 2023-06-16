01b1df403c8a90b58a781921bd9aec7150b7e711
bitsstdcheee <47266377+bitsstdcheee@users.noreply.github.com>
2023-06-16 14:00:13 +0800

    tests: 添加 judge_have 测试 (#28)
    补充 judge_have 测试
    
    补充 wooden_judge have_att 的遗漏分支
    
    修正了代码覆盖无法统计测试的问题
    
    Github Action: Codecov 上传失败时会导致任务失败
    
    LCOV 添加 gen-html
    
    修正 typo
    
    测试修改为使用 ctest
    ---------
    
    Co-authored-by: bitsstdcheee <1009770830@qq.com>

--- tests/judge_have/main.cpp
+++ tests/judge_have/main.cpp
@@ -7,12 +7,29 @@ using namespace tskl;
 
 #define make_weapon(x) make_pair(0, Skill(x, 1))
 
-TEST(have_att_test, BasicAssertions) {
-    EXPECT_TRUE(have_att(make_weapon(tskl::wooden_sword)));
-    EXPECT_TRUE(have_att(make_pair(0, Skill(tskl::yellow_sword, 1))));
-    EXPECT_TRUE(have_att(make_pair(0, Skill(tskl::stone_sword, 1))));
-    EXPECT_TRUE(have_att(make_pair(0, Skill(tskl::iron_sword, 1))));
-    EXPECT_TRUE(have_att(make_pair(0, Skill(tskl::gold_sword, 1))));
-    EXPECT_TRUE(have_att(make_pair(0, Skill(tskl::diamond_sword, 1))));
-    EXPECT_TRUE(have_att(make_pair(0, Skill(tskl::enchanted_sword, 1))));
+TEST(have_att_test, Positive) {
+    EXPECT_EQ(have_att(make_weapon(tskl::wooden_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::yellow_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::stone_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::iron_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::gold_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::diamond_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::enchanted_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::bo_sword)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::hither)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::fist)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::palm)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::tube)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::alpaca)), true);
+    EXPECT_EQ(have_att(make_weapon(tskl::hither)), true);
+}
+
+TEST(have_att_test, Negative) {
+    EXPECT_EQ(have_att(make_weapon(tskl::clap)), false);
+    EXPECT_EQ(have_att(make_weapon(tskl::defense)), false);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
