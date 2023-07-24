9a7e878e302b65c533d4bc7089d8e8cbe7d4ccb9
Dell-G3 <Jestingaero@163.com>
2023-07-24 14:19:57 +0800

    have_axe 去除 clap, 完善 judge_have 测试 添加了对 have_axe, have_clap_axe 的测试

--- tests/judge_have/main.cpp
+++ tests/judge_have/main.cpp
@@ -30,6 +30,29 @@ TEST(have_att_test, Negative) {
     EXPECT_EQ(have_att(make_weapon(tskl::defense)), false);
 }
 
+TEST(have_axe, Positive) {
+    EXPECT_EQ(have_axe(make_weapon(tskl::wooden_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::normal_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::diamond_axe)), true);
+    EXPECT_EQ(have_axe(make_weapon(tskl::enchanted_axe)), true);
+}
+
+TEST(have_axe, Negative) {
+    EXPECT_EQ(have_axe(make_weapon(tskl::clap)), false);
+    EXPECT_EQ(have_axe(make_weapon(tskl::defense)), false);
+    EXPECT_EQ(have_axe(make_weapon(tskl::hither)), false);
+}
+
+TEST(have_clap_axe, Positive) {
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::clap)), true);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::wooden_axe)), true);
+}
+
+TEST(have_clap_axe, Negative) {
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::defense)), false);
+    EXPECT_EQ(have_clap_axe(make_weapon(tskl::hither)), false);
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
