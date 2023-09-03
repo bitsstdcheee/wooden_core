f2e7b4eef80569bdea638b9b014ef9227cfb50bc
bitsstdcheee <Jestingaero@163.com>
2023-09-03 12:04:25 +0800

    修复自动格式化程序
    添加 tests/ 目录
    
    不会对 proto/ 目录下的代码进行格式化
    
    (cherry picked from commit 7e7a61a0e97b545fc24ece4b47665c83ecb67a59)

--- .github/scripts/check_clang_format.sh
+++ .github/scripts/check_clang_format.sh
@@ -4,17 +4,17 @@ echo "当前脚本工作目录: $(pwd)"
 repo_dir=$(git rev-parse --show-toplevel)
 
 file_list=("*.c" "*.h" "*.cpp" "*.inl")
-file_list_test=("${file_list[@]/#/test\/**\/}")
+file_list_test=("${file_list[@]/#/tests\/**\/}")
 file_list_full=("${file_list[@]}" "${file_list_test[@]}")
 
 echo "当前通配符列表: ${file_list_full[@]}"
 
 if [[ -n "$GITHUB_ACTIONS" ]]; then
-    files=$(git ls-files -- "${file_list_full[@]}")
+    files=$(git ls-files -- ${file_list_full[@]})
     # Github Actions 的默认工作目录在项目根目录
 else
     cd ${repo_dir}
-    files=$(git ls-files -- "${file_list_full[@]}")
+    files=$(git ls-files -- ${file_list_full[@]})
 fi
 result=0
 rm -f ./clang-format/clang-format-output.log
